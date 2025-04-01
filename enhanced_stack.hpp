33a41e1bec1df980f5ef90735258084bb655094d
S W <a2901805528@163.com>
2025-04-01 19:02:31 +0800

    explain update

--- enhanced_stack.hpp
+++ enhanced_stack.hpp
@@ -1,3 +1,21 @@
+/**
+ * 文件名: enhanced_stack.cpp
+ * 项目名称: enhanced_stack
+ * 文件创建日期: 2025-03-25
+ * 作者: 007WS
+ * 联系方式: a2901805528@163.com
+ *
+ * 最后编辑日期: 2025-04-01
+ * 最后编辑者: 007WS
+ * 最后编辑者联系方式: a2901805528@163.com
+ *
+ * 文件描述:
+ * C++ STL stack 扩展版本（大学生练手项目）
+ *
+ * 许可证:
+ * 无
+ */
+
 #ifndef ENHANCED_STACK_HPP
 #define ENHANCED_STACK_HPP
 
