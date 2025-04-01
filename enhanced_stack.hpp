586296a21744ec48847e5e28671ec48782953db5
S W <a2901805528@163.com>
2025-04-01 18:12:16 +0800

    auto_ptr

--- enhanced_stack.hpp
+++ enhanced_stack.hpp
@@ -1,27 +1,10 @@
-/**
-* 文件名: enhanced_stack.hpp
- * 项目名称: enhanced_stack
- * 文件创建日期: 2025-03-25
- * 作者: 007WS
- * 联系方式: a2901805528@163.com
- *
- * 最后编辑日期: 2025-03-27
- * 最后编辑者: 007WS
- * 最后编辑者联系方式: a2901805528@163.com
- *
- * 文件描述:
- * C++ STL stack 扩展版本（大学生练手项目）
- *
- * 许可证:
- * 无
- */
-
 #ifndef ENHANCED_STACK_HPP
 #define ENHANCED_STACK_HPP
 
 #include <iostream>
 #include <stdexcept>
 #include <cmath>
+#include <memory>
 
 namespace enhanced {
 
@@ -31,7 +14,7 @@ public:
     enhanced_stack() {
         _size = 0;
         _capacity = 32;
-        _data = new Type[_capacity];
+        _data = std::make_unique<Type[]>(_capacity);
         if (std::is_integral_v<Type>) {
             _check = true;
         }
@@ -39,62 +22,61 @@ public:
             _check = false;
         }
     }
-    ~enhanced_stack() {
-        _size = 0;
-        _capacity = 32;
-        delete[] _data;
-    }
+
+    ~enhanced_stack() = default;
+
     enhanced_stack(const enhanced_stack &other) {
         _size = other._size;
         _capacity = other._capacity;
-        _data = new Type[_capacity];
+        _data = std::make_unique<Type[]>(_capacity);
         _check = other._check;
         for (int i = 0; i < _size; i++) {
             _data[i] = other._data[i];
         }
     }
+
     enhanced_stack &operator=(const enhanced_stack &other) {
-        _size = other._size;
-        _capacity = other._capacity;
-        delete[] _data;
-        _data = new Type[_capacity];
-        for (int i = 0; i < _size; i++) {
-            _data[i] = other._data[i];
+        if (this != &other) {
+            _size = other._size;
+            _capacity = other._capacity;
+            _data = std::make_unique<Type[]>(_capacity);
+            _check = other._check;
+            for (int i = 0; i < _size; i++) {
+                _data[i] = other._data[i];
+            }
         }
         return *this;
     }
-    enhanced_stack(enhanced_stack &&other) noexcept {
-        _size = other._size();
-        _capacity = other._capacity;
-        _data = new Type[_capacity];
-        _check = other._check;
-        for (int i = 0; i < _size; i++) {
-            _data[i] = other._data[i];
-        }
+
+    enhanced_stack(enhanced_stack &&other) noexcept
+        : _size(other._size),
+          _capacity(other._capacity),
+          _data(std::move(other._data)),  // 转移所有权
+          _check(other._check) {
         other._size = 0;
         other._capacity = 32;
-        delete[] other._data;
     }
+
     enhanced_stack &operator=(enhanced_stack &&other) noexcept {
-        _size = other._size;
-        _capacity = other._capacity;
-        delete[] _data;
-        _data = new Type[_capacity];
-        for (int i = 0; i < _size; i++) {
-            _data[i] = other._data[i];
+        if (this != &other) {
+            _size = other._size;
+            _data = std::move(other._data);
+            _capacity = other._capacity;
+            _check = other._check;
+            other._size = 0;
+            other._capacity = 32;
         }
-        other._size = 0;
-        other._capacity = 32;
-        delete[] other._data;
         return *this;
     }
 
     [[nodiscard]] int size() const {
         return _size;
     }
+
     [[nodiscard]] bool empty() const {
         return _size == 0;
     }
+
     Type top() const {
         if (_size == 0) {
             throw std::runtime_error("Error: Stack is empty");
@@ -108,6 +90,7 @@ public:
         }
         _data[_size++] = value;
     }
+
     void push(const Type &left_val, const Type &right_val) {
         if (!_check) {
             throw std::runtime_error("Error: Invalid Type");
@@ -126,12 +109,14 @@ public:
             }
         }
     }
+
     void pop() {
         if (_size == 0) {
             throw std::runtime_error("Error: Stack is empty");
         }
         _size--;
     }
+
     void pop(const int &num) {
         if (_size < num) {
             throw std::runtime_error("Error: Stack size is smaller than your input");
@@ -152,22 +137,23 @@ public:
 
 private:
     int _size;
-    Type *_data;
     int _capacity;
+    std::unique_ptr<Type[]> _data;
     bool _check;
+
     void addCapacity() {
-        Type *tmp_data = new Type[_capacity];
+        auto new_capacity = _capacity * 2;
+        auto new_data = std::make_unique<Type[]>(new_capacity);
+
         for (int i = 0; i < _size; i++) {
-            tmp_data[i] = _data[i];
+            new_data[i] = _data[i];
         }
-        delete[] _data;
-        _data = new Type[_capacity * 2];
-        for (int i = 0; i < _size; i++) {
-            _data[i] = tmp_data[i];
-        }
+
+        _data = std::move(new_data);
+        _capacity = new_capacity;
     }
 };
 
-} // enhanced
+} // namespace enhanced
 
-#endif //ENHANCED_STACK_HPP
+#endif // ENHANCED_STACK_HPP
